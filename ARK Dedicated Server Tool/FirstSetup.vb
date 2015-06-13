85574394fae067205c8c99d1a0c6dbf1451d4880
Andrew A <HellsGuard@live.com>
2015-06-13 14:15:22 -0400

    Version 2.0.0.10

---
+++ ARK Dedicated Server Tool/FirstSetup.vb
@@ -0,0 +1,31 @@
+﻿Public Class FirstSetup
+    Dim path As String
+
+    Private Sub FirstSetup_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If My.Settings.firstTimeRan Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub btnDefaultSetup_Click(sender As Object, e As EventArgs) Handles btnDefaultSetup.Click
+        My.Settings.defaultSetup = True
+        My.Settings.firstTimeRan = True
+        My.Settings.userSetDirectoryBool = False
+        My.Settings.userSetDirectory = Directory.GetCurrentDirectory & "\ark_server\ShooterGame\Saved\Config\WindowsServer\"
+        My.Settings.runningServerDirectory = Directory.GetCurrentDirectory & "\ark_server\ShooterGame\Binaries\Win64\"
+        My.Settings.Save()
+
+        Main.DefaultSetup()
+        Me.Close()
+    End Sub
+
+    Private Sub btnAdvancedSeup_Click(sender As Object, e As EventArgs) Handles btnAdvancedSeup.Click
+        My.Settings.advancedSetup = True
+        My.Settings.firstTimeRan = True
+        My.Settings.userSetDirectoryBool = True
+        My.Settings.Save()
+
+        Main.AdvancedSetup()
+        Me.Close()
+    End Sub
+End Class
