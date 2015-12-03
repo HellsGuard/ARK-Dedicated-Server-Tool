6614cc2d3710dcf8529851fc15cfe402e475b122
James Elliott <james.d.elliott@gmail.com>
2015-12-03 01:31:23 +0000

    MultiValue binding for RCON buffer
    - Moved the Admin Logging checkbox and config
    - Added the AllTrueMultiValueConverter class to the ViewModel
    - Used it for the RCON Admin Log Buffer

---
+++ ARK Server Manager/Lib/ViewModel/AllTrueMultiValueConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace ARK_Server_Manager.Lib.ViewModel
+{
+    public class AllTrueMultiValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            foreach (object value in values)
+            {
+                if (!(value is bool) || !(bool)value)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("AllTrueConverter is a OneWay converter.");
+        }
+    }
+}
