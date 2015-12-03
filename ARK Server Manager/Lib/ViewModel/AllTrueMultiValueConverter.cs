403a2b0a68fa4411ea3f5d8972fc85b7f3001e7f
James Elliott <james.d.elliott@gmail.com>
2015-12-03 02:42:01 +0000

    Use Linq for AllTrueMultiValueConverter
    Using Linq as per https://github.com/james-d-elliott/ARK-Dedicated-Server-Tool/commit/6614cc2d3710dcf8529851fc15cfe402e475b122#commitcomment-14751457

--- ARK Server Manager/Lib/ViewModel/AllTrueMultiValueConverter.cs
+++ ARK Server Manager/Lib/ViewModel/AllTrueMultiValueConverter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Data;
 using System.Globalization;
+using System.Linq;
 
 namespace ARK_Server_Manager.Lib.ViewModel
 {
@@ -8,14 +9,7 @@ namespace ARK_Server_Manager.Lib.ViewModel
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            foreach (object value in values)
-            {
-                if (!(value is bool) || !(bool)value)
-                {
-                    return false;
-                }
-            }
-            return true;
+            return !values.Any(v => !(v is bool) || !(bool)v);
         }
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
