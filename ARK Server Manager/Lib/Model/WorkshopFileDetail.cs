c6709169a14867837c5bbb907c29f28ea8f5db1d
Bletch <bletch1971@hotmail.com>
2016-08-28 15:08:33 +1000

    Workshoop List Changes
    1. Added additional columns to the Workshop list.

--- ARK Server Manager/Lib/Model/WorkshopFileDetail.cs
+++ ARK Server Manager/Lib/Model/WorkshopFileDetail.cs
@@ -180,14 +180,36 @@ namespace ARK_Server_Manager.Lib.Model
     public class WorkshopFileItem : DependencyObject
     {
         public static readonly DependencyProperty AppIdProperty = DependencyProperty.Register(nameof(AppId), typeof(string), typeof(WorkshopFileItem), new PropertyMetadata(string.Empty));
+        public static readonly DependencyProperty CreatedDateProperty = DependencyProperty.Register(nameof(CreatedDate), typeof(DateTime), typeof(WorkshopFileItem), new PropertyMetadata(DateTime.MinValue));
+        public static readonly DependencyProperty FileSizeProperty = DependencyProperty.Register(nameof(FileSize), typeof(long), typeof(WorkshopFileItem), new PropertyMetadata(-1L));
+        public static readonly DependencyProperty SubscriptionsProperty = DependencyProperty.Register(nameof(Subscriptions), typeof(int), typeof(WorkshopFileItem), new PropertyMetadata(0));
         public static readonly DependencyProperty TimeUpdatedProperty = DependencyProperty.Register(nameof(TimeUpdated), typeof(int), typeof(WorkshopFileItem), new PropertyMetadata(0));
         public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(nameof(Title), typeof(string), typeof(WorkshopFileItem), new PropertyMetadata(string.Empty));
+        public static readonly DependencyProperty UpdatedDateProperty = DependencyProperty.Register(nameof(UpdatedDate), typeof(DateTime), typeof(WorkshopFileItem), new PropertyMetadata(DateTime.MinValue));
         public static readonly DependencyProperty WorkshopIdProperty = DependencyProperty.Register(nameof(WorkshopId), typeof(string), typeof(WorkshopFileItem), new PropertyMetadata(string.Empty));
 
         public string AppId
         {
             get { return (string)GetValue(AppIdProperty); }
             set { SetValue(AppIdProperty, value); }
+        }
+
+        public DateTime CreatedDate
+        {
+            get { return (DateTime)GetValue(CreatedDateProperty); }
+            set { SetValue(CreatedDateProperty, value); }
+        }
+
+        public long FileSize
+        {
+            get { return (long)GetValue(FileSizeProperty); }
+            set { SetValue(FileSizeProperty, value); }
+        }
+
+        public int Subscriptions
+        {
+            get { return (int)GetValue(SubscriptionsProperty); }
+            set { SetValue(SubscriptionsProperty, value); }
         }
 
         public int TimeUpdated
@@ -205,6 +227,12 @@ namespace ARK_Server_Manager.Lib.Model
 
                 TitleFilterString = value?.ToLower();
             }
+        }
+
+        public DateTime UpdatedDate
+        {
+            get { return (DateTime)GetValue(UpdatedDateProperty); }
+            set { SetValue(UpdatedDateProperty, value); }
         }
 
         public string WorkshopId
@@ -229,9 +257,18 @@ namespace ARK_Server_Manager.Lib.Model
 
             var result = new WorkshopFileItem();
             result.AppId = item.creator_appid;
+            result.CreatedDate = ModUtils.UnixTimeStampToDateTime(item.time_created);
+            result.FileSize = -1;
+            result.Subscriptions = item.subscriptions;
             result.TimeUpdated = item.time_updated;
             result.Title = item.title ?? string.Empty;
+            result.UpdatedDate = ModUtils.UnixTimeStampToDateTime(item.time_updated);
             result.WorkshopId = item.publishedfileid ?? string.Empty;
+
+            long fileSize;
+            if (long.TryParse(item.file_size, out fileSize))
+                result.FileSize = fileSize;
+
             return result;
         }
     }
