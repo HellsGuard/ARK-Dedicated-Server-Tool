84c1a777004ed5f8c87bb3b074a7e5821bb55341
Bletch <bletch1971@hotmail.com>
2017-09-07 12:18:16 +1000

    Auto-Backup/Update Setting Changes
    1. Removed the validation check for the auto-backup and auto-update
    interval.
    2. Changed the Time to Minute converter to handle hours > 23.

--- ARK Server Manager/Common/Converters/MinutesToTimeValueConverter.cs
+++ ARK Server Manager/Common/Converters/MinutesToTimeValueConverter.cs
@@ -6,12 +6,15 @@ namespace ARK_Server_Manager.Lib.ViewModel
 
     public class MinutesToTimeValueConverter : IValueConverter
     {
+        public const int MAX_VALUE_HOURS = 24 * 365;
+        public const int MAX_VALUE_MINUTES = 59;
+
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             // Value is seconds since midnight.
             var totalMinutes = (int)value;
-            var hours = Math.Min(Math.Max(totalMinutes / 60, 0), 23);
-            var minutes = Math.Min(Math.Max(totalMinutes % 60, 0), 59);
+            var hours = Math.Min(Math.Max(totalMinutes / 60, 0), MAX_VALUE_HOURS);
+            var minutes = Math.Min(Math.Max(totalMinutes % 60, 0), MAX_VALUE_MINUTES);
             return String.Format("{0:00}:{1:00}", hours, minutes);
         }
 
