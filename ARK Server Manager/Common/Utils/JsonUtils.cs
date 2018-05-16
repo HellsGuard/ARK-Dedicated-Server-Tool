7e0c6377335753aaa308b08d340d7e91b17f95fd
Bletch <bletch1971@hotmail.com>
2018-05-16 15:55:39 +1000

    User Settings 1. added a new user settings backup when ASm is closed.

--- ARK Server Manager/Common/Utils/JsonUtils.cs
+++ ARK Server Manager/Common/Utils/JsonUtils.cs
@@ -4,13 +4,15 @@ namespace ARK_Server_Manager.Lib
 {
     public static class JsonUtils
     {
-        public static string Serialize<T>(T value)
+        public static string Serialize<T>(T value, JsonSerializerSettings settings = null)
         {
             if (value == null)
                 return string.Empty;
 
             try
             {
+                if (settings != null)
+                    return JsonConvert.SerializeObject(value, Formatting.Indented, settings);
                 return JsonConvert.SerializeObject(value, Formatting.Indented);
             }
             catch
@@ -19,14 +21,14 @@ namespace ARK_Server_Manager.Lib
             }
         }
 
-        public static bool Serialize<T>(T value, string filename)
+        public static bool Serialize<T>(T value, string filename, JsonSerializerSettings settings = null)
         {
             if (value == null)
                 return false;
 
             try
             {
-                var jsonString = Serialize(value);
+                var jsonString = Serialize(value, settings);
                 System.IO.File.WriteAllText(filename, jsonString);
                 return true;
             }
@@ -36,13 +38,15 @@ namespace ARK_Server_Manager.Lib
             }
         }
 
-        public static T Deserialize<T>(string jsonString)
+        public static T Deserialize<T>(string jsonString, JsonSerializerSettings settings = null)
         {
             if (string.IsNullOrEmpty(jsonString))
                 return default(T);
 
             try
             {
+                if (settings != null)
+                    return JsonConvert.DeserializeObject<T>(jsonString, settings);
                 return JsonConvert.DeserializeObject<T>(jsonString);
             }
             catch
@@ -51,14 +55,14 @@ namespace ARK_Server_Manager.Lib
             }
         }
 
-        public static T DeserializeFromFile<T>(string file)
+        public static T DeserializeFromFile<T>(string file, JsonSerializerSettings settings = null)
         {
             if (string.IsNullOrEmpty(file) || !System.IO.File.Exists(file))
                 return default(T);
 
             try
             {
-                return Deserialize<T>(System.IO.File.ReadAllText(file));
+                return Deserialize<T>(System.IO.File.ReadAllText(file), settings);
             }
             catch
             {
